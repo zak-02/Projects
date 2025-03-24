20540321f6ada8d2b8069da25e79c5205ffb8a94
Zuckareya Amin <aminzak1234@gmail.com>
2025-03-24 20:42:31 +0000

    add title

--- Fashion app/app/lib/main.dart
+++ Fashion app/app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fashion/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    home: MainPage(), // Make sure MainPage is defined and imported correctly
     );
   }
 }
+
+
